b819db41e0cbc42fff015dbb4a338ba5e9c7621c
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-02-12 10:28:11 -0500

    hourly emp class

---
+++ Week6MidtermReview/HourlyEmployee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6MidtermReview
+{
+    public class HourlyEmployee : Employee
+    {
+        public int Hours { get; set; }
+        public double Wage { get; set; }
+
+        public HourlyEmployee(string name, int hours, double wage) : base(name)
+        {
+            Hours = hours;
+            Wage = wage;
+        }
+
+        public override double GrossEarnings
+        {
+            get
+            {
+                if (Hours <= 40)
+                    return Hours * Wage;
+                else
+                    return (40 * Wage) + (Hours - 40) * Wage * 1.5;
+            }
+        }
+    }
+}
