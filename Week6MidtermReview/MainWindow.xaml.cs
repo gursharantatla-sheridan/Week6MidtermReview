c27b28fad6a225504f0e904c84e27bb01abea22c
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-02-12 11:46:34 -0500

    listbox selected name

--- Week6MidtermReview/MainWindow.xaml.cs
+++ Week6MidtermReview/MainWindow.xaml.cs
@@ -66,5 +66,46 @@ namespace Week6MidtermReview
             // add the emp object to the List<>
             _employees.Add(_emp);
         }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            rdoHourly.IsChecked = true;
+            txtName.Text = txtInput2.Text = txtInput3.Text = "";
+            txtGrossEarnings.Text = txtTax.Text = txtNetEarnings.Text = "";
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void lstEmployees_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lstEmployees.SelectedItem != null)
+            {
+                _emp = _employees[lstEmployees.SelectedIndex];
+
+                txtName.Text = _emp.Name;
+
+                if (_emp is HourlyEmployee hrEmp)
+                {
+                    //HourlyEmployee hrEmp = (HourlyEmployee)_emp;
+
+                    txtInput2.Text = hrEmp.Hours.ToString();
+                    txtInput3.Text = hrEmp.Wage.ToString();
+                    rdoHourly.IsChecked = true;
+                }
+                else if (_emp is CommissionEmployee commEmp)
+                {
+                    txtInput2.Text = commEmp.GrossSales.ToString();
+                    txtInput3.Text = commEmp.CommissionRate.ToString();
+                    rdoCommission.IsChecked = true;
+                }
+
+                txtGrossEarnings.Text = _emp.GrossEarnings.ToString("C");
+                txtTax.Text = _emp.Tax.ToString("C");
+                txtNetEarnings.Text = _emp.NetEarnings.ToString("C");
+            }
+        }
     }
 }
