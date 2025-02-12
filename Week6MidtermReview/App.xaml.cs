32ad3c32fa0ff8910270e92e0fd277ea56b36dae
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-02-12 10:13:33 -0500

    Add project files.

---
+++ Week6MidtermReview/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Week6MidtermReview
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
